37cd9e0d3061d71afa9b48015650504d4d3fe3b0
Dimitar Mitrevski <71487915+DimitarMitrevski@users.noreply.github.com>
2022-07-03 16:01:19 +0200

    Previewing people who joined and submitted a solution, ready.

---
+++ src/components/JoinedCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="w-full bg-white p-5 rounded shadow flex items-center justify-between mb-2"
+  >
+    <div class="inline-flex items-center">
+      <img
+        :src="join.photoURL || '/defaultUser.svg'"
+        style="width: 75px; height: 75px"
+        class="bg-gray-400 rounded-full object-fill"
+      />
+      &nbsp; {{ join.displayName }}
+    </div>
+    <div class="inline float-right">
+      <p class="font-medium text-gray-600">
+        Joined on: <span class="font-bold">{{ dateJoined }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["join"],
+  computed: {
+    dateJoined() {
+      const date = new Date(this.join.timestamp);
+      let month = date.getMonth() + 1;
+      month = month <= 9 ? `0${month}` : month;
+      let day = date.getDate();
+      day = day <= 9 ? `0${day}` : day;
+
+      return `${day}/${month}/${date.getFullYear()}`;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/SubmitedCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="w-full bg-white p-5 rounded shadow flex items-center justify-between mb-2"
+  >
+    <div class="inline-flex items-center">
+      <img
+        :src="submission.photoURL || '/defaultUser.svg'"
+        style="width: 75px; height: 75px"
+        class="bg-gray-400 rounded-full object-fill"
+      />
+      &nbsp; {{ submission.displayName }}
+    </div>
+    <div class="inline float-right">
+      <a
+        :href="submission.solution"
+        class="font-medium text-indigo-600 hover:text-indigo-500"
+        target="_blank"
+        >View</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["submission"],
+};
+</script>
+
+<style></style>
